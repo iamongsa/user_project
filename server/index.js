4242b5cd286343d1c8f0e63f7b9d763a20db5ef4
iamongsa <sudarat15fah@gmail.com>
2023-11-10 21:46:09 +0700

    อัพเดตรายการซ่อม

--- server/index.js
+++ server/index.js
@@ -10,11 +10,11 @@ const db = mysql.createConnection({
   user: "root",
   host: "localhost",
   password: "",
-  database: "changdum",
+  database: "edit_admin_project",
 });
 
 app.listen(3005, () => {
-  console.log("Hey , yoour server is running on port 3005!");
+  console.log("Hey , your server is running on port 3005!");
 });
 
 app.get("/datastatus", (req, res) => {});
@@ -29,24 +29,29 @@ app.get("/customerData/:phone/:plate", (req, res) => {
   db.query(phoneSql, [phone], (phoneErr, phoneResult) => {
     if (phoneErr) {
       console.log(phoneErr);
-      res.status(500).json({ error: 'เกิดข้อผิดพลาดในการค้นหาหมายเลขโทรศัพท์' });
+      res
+        .status(500)
+        .json({ error: "เกิดข้อผิดพลาดในการค้นหาหมายเลขโทรศัพท์" });
     } else if (phoneResult.length > 0) {
       const customer_id = phoneResult[0].cus_id;
 
       db.query(plateSql, [plate], (plateErr, plateResult) => {
         if (plateErr) {
           console.log(plateErr);
-          res.status(500).json({ error: 'เกิดข้อผิดพลาดในการค้นหาหมายเลขทะเบียนรถ' });
+          res
+            .status(500)
+            .json({ error: "เกิดข้อผิดพลาดในการค้นหาหมายเลขทะเบียนรถ" });
         } else if (plateResult.length > 0) {
           const car_id = plateResult[0].car_id;
 
           db.query(
-            `SELECT order_repair.order_id, order_repair.description, order_repair.estimate_time, customers.name, customers.phone, cars.plate_license, mechanic.mech_name, status_order.status_name, MAX(order_repair.create_order) AS last_create_order
+            `SELECT order_detail.estimate_price,order_detail.status_update,order_detail.update_date,order_detail.repair_description, order_repair.order_id, order_repair.description, order_repair.estimate_time, customers.name, customers.phone, cars.plate_license, mechanic.mech_name, status_order.status_name, MAX(order_repair.create_order) AS last_create_order
             FROM order_repair
             INNER JOIN customers ON order_repair.customer_id = customers.cus_id
             INNER JOIN cars ON order_repair.car_id = cars.car_id
             INNER JOIN mechanic ON order_repair.mechanic_id = mechanic.mech_id
             INNER JOIN status_order ON order_repair.status_id = status_order.status_id
+            INNER JOIN order_detail ON order_repair.order_id = order_detail.order_id
             WHERE order_repair.customer_id = ? AND order_repair.car_id = ?
             GROUP BY order_repair.order_id, order_repair.description, order_repair.estimate_time, customers.name, customers.phone, cars.plate_license, mechanic.mech_name, status_order.status_name
             ORDER BY last_create_order DESC
@@ -56,18 +61,20 @@ app.get("/customerData/:phone/:plate", (req, res) => {
             (err, result) => {
               if (err) {
                 console.log(err);
-                res.status(500).json({ error: 'เกิดข้อผิดพลาดในการค้นหาข้อมูลรายการซ่อม' });
+                res
+                  .status(500)
+                  .json({ error: "เกิดข้อผิดพลาดในการค้นหาข้อมูลรายการซ่อม" });
               } else {
                 res.json(result);
               }
             }
           );
         } else {
-          res.status(404).json({ error: 'ไม่พบข้อมูลรถที่ระบุ' });
+          res.status(404).json({ error: "ไม่พบข้อมูลรถที่ระบุ" });
         }
       });
     } else {
-      res.status(404).json({ error: 'ไม่พบข้อมูลลูกค้าที่ระบุ' });
+      res.status(404).json({ error: "ไม่พบข้อมูลลูกค้าที่ระบุ" });
     }
   });
 });
